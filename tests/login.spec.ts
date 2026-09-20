2930d9f0e2f1637589d2119295104cff760cbdd4
Thiago Luiz <thiago-470@outlook.com>
2026-09-19 21:52:53 -0300

    testing login with invalid credentials

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -13,3 +13,17 @@ test('Login with valid credentials', async ({ page }) => {
     const productsTitle = await page.locator('.header_secondary_container > span');
     await expect(productsTitle).toHaveText('Products');
 });
+
+test('Login with invalid credentials', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await expect(await page.title()).toBe('Swag Labs');
+
+    await page.locator('[data-test="username"]').fill('invalid_user');
+    await page.locator('[data-test="password"]').fill('invalid_password');
+    await page.locator('[data-test="login-button"]').click();
+
+    const errorMessage = await page.getByText('Epic sadface: Username and password do not match any user in this service');
+    await expect(errorMessage).toBeVisible();
+
+    await page.locator('[data-test="error-button"]').click();
+});
